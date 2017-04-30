84d61ccf0b9e0f7270b685959ca59a5af21bcfcb
szemyxz <psorbal@gmail.com>
2017-04-30 13:18:16 +0200

    Create register part in project

--- src/main/java/pl/filmbox/controllers/AuthController.java
+++ src/main/java/pl/filmbox/controllers/AuthController.java
@@ -1,14 +1,49 @@
 package pl.filmbox.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import pl.filmbox.models.User;
+import pl.filmbox.services.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class AuthController {
 
+    private UserService userService;
+
+    @Autowired
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/login")
     public ModelAndView getLoginForm() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("auth/login");
+        return modelAndView;
+    }
+
+    @GetMapping("/register")
+    public ModelAndView getRegisterForm() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("user", new User());
+        modelAndView.setViewName("auth/register");
+        return modelAndView;
+    }
+
+    @PostMapping("/register")
+    public ModelAndView register(
+            @ModelAttribute User user) {
+        List<Long> credits = new ArrayList<>();
+        credits.add(2L);
+        userService.addUser(user, credits);
+
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("auth/login");
         return modelAndView;
